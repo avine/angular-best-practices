0ec5590e04b1d5a51d10881573daa268b5f714b5
Stéphane Francel <contact@avine.io>
2018-07-12 09:45:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# NgBestPractices
+
+Project scaffolding:
+
+```bash
+ng new ng-best-practices --prefix nbp --style scss --routing
+```
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nbp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'nbp';
+}
